8d5dfd63650bb9935018935c7c01ad7f2b8e22ae
nosov33 <ivan@inosov.ru>
2015-10-23 17:35:37 +0300

    New theme

---
+++ templates/page--front.tpl.php
@@ -0,0 +1,189 @@
+<div class="page">
+<div class="container">
+	<div class="col-lg-6 logos">
+		<center><img src="http://estate2015.com/sites/default/files/logo.jpg" height="50" /></center>
+	</div>
+	<div class="col-lg-6 logos">
+		<center><img src="http://estate2015.com/sites/default/files/logo_4_0.png" height="50" /></center>
+	</div>
+	<br class="clear" />
+		<div class="shapka">
+		<div class="bodys">
+<H1>КУДА ВКЛАДЫВАТЬ ДЕНЬГИ<br />В 2016 ГОДУ!</H1>
+<!-- Button trigger modal -->
+<center><a type="button" class="btn btn-primary btn-lg" href="#prereg">
+  ЗАРЕГИСТРИРОВАТЬСЯ СЕЙЧАС
+</a></center>
+		</div>
+		</div>
+</div>
+<div class="delimiter">
+	<h2>УНИКАЛЬНЫЙ СЕМИНАР В ВАШЕМ ГОРОДЕ!</h2>
+</div>
+<div class="container pad">
+	<div class="col-lg-8 col-md-6">
+	<?php if ($page['why_1']): ?>
+		<?php print render($page['why_1']); ?>
+	<?php endif; ?>	
+	</div>
+	<div class="col-lg-4 col-md-6">
+		<img src="http://estate2015.com/sites/default/files/man.jpg" style="width: 100%; height: auto;" />
+	</div>
+</div>
+<div class="container pat4 pad">
+	<h2 class="red">ЛУЧШИЕ СТРАНЫ ДЛЯ ИНВЕСТИРОВАНИЯ:</h2>
+</div>
+<div class="container pad">
+	<div class="col-lg-2 col-md-2">
+		<center><img src="http://estate2015.com/sites/default/files/turkey.jpg" class="img-circle" /></center><br />
+	</div>
+	<div class="col-lg-4 col-md-4">
+		<h3>Турция</h3>
+		Самая быстроразвивающаяся экономика в Европе. 2-е место в мире среди наиболее привлекательных направлений для размещения инвестиций в недвижимость и получение дохода в евро или $ (Global House Price Index).
+	</div>
+	<div class="col-lg-2 col-md-2">
+		<center><img src="http://estate2015.com/sites/default/files/bolg.jpg" class="img-circle" /></center><br />
+	</div>
+	<div class="col-lg-3 col-md-3">
+		<h3>Болгария</h3>
+		Невысокая себестоимость строительства и возможность получить  ВНЖ и ПМЖ в Европе делает Болгарию одной из стран для выгодных вложений.
+	</div>
+	<div class="col-lg-2 col-md-2"></div>
+	<br class="clear" />
+	<div class="col-lg-2 col-md-2">
+		<center><img src="http://estate2015.com/sites/default/files/cipr.jpg" class="img-circle" /></center><br />
+	</div>
+	<div class="col-lg-4 col-md-4">
+		<h3>Кипр</h3>
+		Безопасный, уютный, гостеприимный остров, возможность получения долгосрочной визы и ВНЖ в Европе.
+	</div>
+	<div class="col-lg-2 col-md-2">
+		<center><img src="http://estate2015.com/sites/default/files/spain.jpg" class="img-circle" /></center><br />
+	</div>
+	<div class="col-lg-3 col-md-3">
+		<h3>Испания</h3>
+		Значительное снижение цен на недвижимость (почти на 40% с 2008г.), получение  ВНЖ и ПМЖ в Европе.
+	</div>
+	<div class="col-lg-2 col-md-2"></div>
+</div>
+<div class="container pat4 pad">
+	<h2>ГОРОДА ПРОВЕДЕНИЯ:</h2>
+</div>
+<div class="container">
+	<div class="col-lg-6 col-md-6 dates">
+	<img src="http://estate2015.com/sites/default/files/hark.jpg" style="width: 100%; height: auto;" />
+		<h3>3 ноября в 18.00 Семинар в Харькове</h3>
+	<img src="http://estate2015.com/sites/default/files/odessa.jpg" style="width: 100%; height: auto;" />
+		<h3>10 ноября Семинар в Одессе</h3>
+	</div>
+	<div class="col-lg-6 col-md-6 dates">
+	<img src="http://estate2015.com/sites/default/files/dnepr.jpg" style="width: 100%; height: auto;" />
+		<h3>5 ноября в 18.00 Семинар в Днепропетровске</h3>
+	<img src="http://estate2015.com/sites/default/files/herson.jpg" style="width: 100%; height: auto;" />
+		<h3>12 ноября Семинар в Херсоне</h3>
+	</div>
+</div>
+<div class="container pat4 pad">
+	<h2 class="red">НАШИ ЭКСПЕРТЫ:</h2>
+</div>
+<div class="container pad experts city">
+	<div class="col-lg-4 col-md-4">
+		<img src="http://estate2015.com/sites/default/files/mevlutbey.jpg" alt="..." class="img-thumbnail"><br />
+		<h4>MEVLUT GORUCU</h4>
+		<strong>Соучредитель компании "Elite Group"</strong>
+	</div>
+	<div class="col-lg-4 col-md-4">
+		<img src="http://estate2015.com/sites/default/files/mustafa.jpg" alt="..." class="img-thumbnail"><br />	
+		<h4>MUSTAFA TOKSOZ</h4>
+		<strong>Соучредитель компании "Elite Group"</strong>
+	</div>
+	<div class="col-lg-4 col-md-4">
+		<img src="http://estate2015.com/sites/default/files/vadim.jpg" alt="..." class="img-thumbnail"><br />
+		<h4>ВАДИМ ИСМАИЛОВ</h4>
+		<strong>Организатор семинара</strong>
+	</div>
+</div>
+
+<div class="container pad">
+	<div class="col-lg-5 col-md-6">
+		<center><img src="http://estate2015.com/sites/default/files/kampanya.jpg" style="width: 60%; height: auto; margin: 10px 0;" /></center>
+		<center><h3>СТОИМОСТЬ СЕМИНАРА: 300 грн.</h3><strong>УСПЕЙ ЗАРЕГИСТРИРОВАТЬСЯ БЕСПЛАТНО!</strong></center><br />
+		<center><script type="text/javascript" src="http://www.timegenerator.ru/s/8e61c7e22de636e39f6d4b3098a308d3.js"></script></center>
+	</div>
+	<div class="col-lg-7 col-md-6">
+		<h2 class="red">УСПЕЙ ЗАРЕГИСТРИРОВАТЬСЯ БЕСПЛАТНО!</h2>
+			<?php if ($page['contact']): ?>
+				<?php print render($page['contact']); ?>
+			<?php endif; ?>			
+	</div>
+</div>
+<div class="delimiter">
+	<h2>КУДА ИНВЕСТИРОВАТЬ СРЕДСТВА В 2016 ГОДУ?</h2>
+</div>
+<div class="container pat4">
+	<div class="col-lg-6 col-md-6">
+			<?php if ($page['addons']): ?>
+				<?php print render($page['addons']); ?>
+			<?php endif; ?>	
+	</div>
+	<div class="col-lg-6 col-md-6">
+			<?php if ($page['slider']): ?>
+				<?php print render($page['slider']); ?>
+			<?php endif; ?>	
+	</div>
+</div>
+<div class="container">
+	<div class="col-lg-6 col-md-6 dates">
+		<img src="http://estate2015.com/sites/default/files/bg.jpg" style="width: 100%; height: auto; margin: 10px 0;" />
+	</div>
+	<div class="col-lg-6 col-md-6 right">
+			<?php if ($page['news']): ?>
+				<?php print render($page['news']); ?>
+			<?php endif; ?>			
+	</div>
+</div>
+<div class="delimiter">
+	<h2>КТО УЖЕ ПОСЕТИЛ ИНВЕСТИЦИОННЫЕ СЕМИНАРЫ</h2>
+</div>
+<div class="container pat4 pad">
+	<div class="col-lg-8 col-md-8">
+			<?php if ($page['testim']): ?>
+				<?php print render($page['testim']); ?>
+			<?php endif; ?>	
+	</div>
+	<div class="col-lg-4 col-md-4">
+		<center><img src="http://estate2015.com/sites/default/files/peoples.png" style="width: 100%; height: auto; margin: 10px 0;" /></center>
+	</div>
+</div>
+<div class="container pad">
+			<?php if ($page['contact_details']): ?>
+				<?php print render($page['contact_details']); ?>
+			<?php endif; ?>		
+</div>
+<div class="container pad">
+	<h2 class="red">СТАНЬТЕ ОДНИМ ИЗ ТЕХ, КТО СДЕЛАЛ ПРАВИЛЬНЫЙ ВЫБОР<br />В ПОЛЬЗУ ИНВЕСТИЦИЙ В НЕДВИЖИМОСТЬ ЗА РУБЕЖОМ!</h2>
+</div>
+<div class="container city pad">
+	<h2>До семинара осталось:</h2>
+	<center class="padding"><script src="http://megatimer.ru/s/fd0427c7fa182461fc9c3031308581a6.js"></script></center>
+</div>
+<div class="delimiter">
+	<h2>УСПЕЙТЕ ЗАРЕГИСТРИРОВАТЬСЯ</h2>
+</div>
+<div class="container pad" id="prereg">
+	<div class="col-lg-2 col-md-2"></div>
+	<div class="col-lg-8 col-md-8">
+		<center><strong>Запишитесь на семинар по инвестициям и станьте одним из счастливчиков, которые отправятся в БЕСПЛАТНЫЙ обзорный тур по объектам недвижимости в Турцию или БОЛГАРИЮ!<br />Количество мест ограничено!</strong></center>
+		<center><strong>КОЛИЧЕСТВО БЕСПЛАТНЫХ ПРИГЛАСИТЕЛЬНЫХ ОГРАНИЧЕНО</strong></center>
+		<center><h3>СТОИМОСТЬ СЕМИНАРА: <strike>300</strike> грн.</h3></center>
+		<h2 class="red">УСПЕЙ ЗАРЕГИСТРИРОВАТЬСЯ БЕСПЛАТНО!</h2>
+			<?php if ($page['contact']): ?>
+				<?php print render($page['contact']); ?>
+			<?php endif; ?>			
+	</div>
+	<div class="col-lg-2 col-md-2"></div>
+</div>
+<footer>
+	2015 &copy; Семинар "КУДА ВКЛАДЫВАТЬ ДЕНЬГИ В 2016 ГОДУ!"
+</footer>
+</div>
---
+++ templates/page.tpl.php
@@ -0,0 +1,37 @@
+<div class="page">
+<div class="container">
+	<div class="col-lg-6 logos">
+		<center><img src="http://estate2015.com/sites/default/files/logo.jpg" height="50" /></center>
+	</div>
+	<div class="col-lg-6 logos">
+		<center><img src="http://estate2015.com/sites/default/files/logo_4_0.png" height="50" /></center>
+	</div>
+	<br class="clear" />
+		<div class="shapka">
+		<div class="bodys">
+<H1>КУДА ВКЛАДЫВАТЬ ДЕНЬГИ<br />В 2016 ГОДУ!</H1>
+		</div>
+		</div>
+</div>
+<div class="delimiter">
+	<h2><?php print $title; ?></h2>
+</div>
+<div class="container pad2">
+			<?php if ($tabs):?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+			<!-- EOF: #tabs -->
+			<?php if (isset($page['content'])) { print render($page['content']); } ?>
+			      <script type="text/javascript">(function() {
+      if (window.pluso) {return};
+       var d = document, s = d.createElement('script'), g = 'getElementsByTagName';
+       s.type = 'text/javascript'; s.charset='UTF-8'; s.async = true;
+       s.src = ('https:' == window.location.protocol ? 'https' : 'http')  + '://x.pluso.ru/pluso-x.js';
+       var h=d[g]('body')[0];
+       h.appendChild(s);
+    })();
+ </script>
+<center><div class="pluso-engine" pluso-sharer={"buttons":"vkontakte,odnoklassniki,facebook,twitter,google,evernote,linkedin,moimir,print,more","style":{"size":"big","shape":"square-corner","theme":"theme03","css":""},"orientation":"horizontal","multiline":true}  data-title="%D0%9A%D0%A3%D0%94%D0%90%20%D0%92%D0%9A%D0%9B%D0%90%D0%94%D0%AB%D0%92%D0%90%D0%A2%D0%AC%20%D0%94%D0%95%D0%9D%D0%AC%D0%93%D0%98%20%D0%92%202016%20%D0%93%D0%9E%D0%94%D0%A3!" data-description="%D0%98%D0%BD%D1%84%D0%BB%D1%8F%D1%86%D0%B8%D1%8F%2C%20%D0%BB%D0%B8%D0%BA%D0%B2%D0%B8%D0%B4%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D0%B8%D0%BC%D1%83%D1%89%D0%B5%D1%81%D1%82%D0%B2%D0%B0%20%D0%BF%D0%B0%D0%B4%D0%B0%D0%B5%D1%82%2C%20%D0%BA%D1%80%D0%B5%D0%B4%D0%B8%D1%82%D1%8B%20%D0%B4%D0%BE%D1%80%D0%BE%D0%B3%D0%B8%D0%B5%2C%20%D0%BD%D0%B5%D1%81%D1%82%D0%B0%D0%B1%D0%B8%D0%BB%D1%8C%D0%BD%D0%BE%D1%81%D1%82%D1%8C%20%D0%BA%D1%83%D1%80%D1%81%D0%B0%2C%20%D0%BD%D0%B5%D0%B2%D0%BE%D0%B7%D0%BC%D0%BE%D0%B6%D0%BD%D0%BE%20%D0%BE%D1%82%D0%BB%D0%BE%D0%B6%D0%B8%D1%82%D1%8C%20%D1%81%D0%B1%D0%B5%D1%80%D0%B5%D0%B6%D0%B5%D0%BD%D0%B8%D1%8F%2C%20%D0%B1%D0%B0%D0%BD%D0%BA%D0%B8%20%D0%B7%D0%B0%D0%BA%D1%80%D1%8B%D0%B2%D0%B0%D1%8E%D1%82%D1%81%D1%8F.%20%D0%94%D0%BE%D1%81%D1%82%D0%B0%D0%BB%D0%BE!%20%D0%9F%D0%BE%20%D0%BF%D1%80%D0%BE%D0%B3%D0%BD%D0%BE%D0%B7%D0%B0%D0%BC%20%D0%9C%D0%B5%D0%B6%D0%B4%D1%83%D0%BD%D0%B0%D1%80%D0%BE%D0%B4%D0%BD%D0%BE%D0%B3%D0%BE%20%D0%B2%D0%B0%D0%BB%D1%8E%D1%82%D0%BD%D0%BE%D0%B3%D0%BE%20%D1%84%D0%BE%D0%BD%D0%B4%D0%B0%2C%20%D0%BF%D0%BE%D1%82%D1%80%D0%B5%D0%B1%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D1%81%D0%BA%D0%B0%D1%8F%20%D0%B8%D0%BD%D1%84%D0%BB%D1%8F%D1%86%D0%B8%D1%8F%20%D0%B2%20%D0%A3%D0%BA%D1%80%D0%B0%D0%B8%D0%BD%D0%B5%20%D0%BF%D0%BE%20%D0%B8%D1%82%D0%BE%D0%B3%D0%B0%D0%BC%202015%20%D0%B3%D0%BE%D0%B4%D0%B0%20%D1%81%D0%BE%D1%81%D1%82%D0%B0%D0%B2%D0%B8%D1%82%20%D0%BC%D0%B8%D0%BD%D1%83%D1%81%2046%25.%20%D0%95%D1%81%D1%82%D1%8C%20%D1%80%D0%B5%D1%88%D0%B5%D0%BD%D0%B8%D0%B5!" data-url="http%3A%2F%2Festate2015.com%2F" data-image="http%3A%2F%2Festate2015.com%2Fsites%2Fall%2Fthemes%2Felite_landing%2Fimages%2Fbg2.jpg"></div> </center>
+</div>
+<footer>
+	2015 &copy; Семинар "КУДА ВКЛАДЫВАТЬ ДЕНЬГИ В 2016 ГОДУ!"
+</footer>
+</div>
